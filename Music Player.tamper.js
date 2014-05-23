42087df1ab9b98fc69912ca9105a2e040ed8f15b
Teddy Sterne <sternetj@rose-hulman.edu>
2014-05-23 02:37:32 -0400

    next button and song count window added

--- Music Player.tamper.js
+++ Music Player.tamper.js
@@ -3,14 +3,9 @@
 // @namespace  http://www.allaccess.com/top40-mainstream/cool-new-music
 // @version    0.1
 // @description  enter something useful
-// @match      http://www.allaccess.com/top40-mainstream/cool-new-music
+// @match      http://www.allaccess.com/*/cool-new-music
 // @copyright  2012+, You
 // ==/UserScript==
-
-// var inject = '<div style="position:fixed;top:5px;right:5px;width:75px;height:75px;z-index:100;background:#eb295c;text-align:center;vertical-align:center;">';
-// inject += '<p style="color:#FFF;">Test</p>';
-// inject += '</div>';
-// document.body.innerHTML += inject;
 
 // get all 'play' class links
 var links = document.getElementsByClassName('play'); // get all links
@@ -35,6 +30,26 @@ links[index].dispatchEvent(theEvent);
 
 setTimeout(musicplayer1, 10000);
 
+var elemDiv = document.createElement('div');
+elemDiv.style.cssText = 'position:fixed;top:5px;right:5px;width:145px;height:80px;z-index:10000;background:#eb295c;';
+elemDiv.style.cssText += 'text-align:center;vertical-align: middle;border-radius:8px;';
+//elemDiv.appendChild(elemP);
+document.body.appendChild(elemDiv);
+
+var para = document.createElement("p");
+para.style.cssText = 'padding-top:10px;padding-bottom:10px;font-weight:bold;font-family:Arial,Helvetica,sans-serif;color:#FFF;font-size: 16px;';
+var node = document.createTextNode('Song: 1 of ' + links.length);
+para.appendChild(node);
+elemDiv.appendChild(para);
+
+var anc = document.createElement("a");
+anc.style.cssText = 'padding-top:10px;font-weight:bold;font-family:Arial,Helvetica,sans-serif;color:#FFF;cursor: pointer; cursor: hand;font-size: 20px;';
+anc.innerHTML = 'Next';
+anc.addEventListener('click',doOverride,false);
+elemDiv.appendChild(anc);
+
+var override = false;
+
 
 function musicplayer1()
 {
@@ -42,6 +57,12 @@ function musicplayer1()
     //alert(player);
     var playing = player.isPlaying()
     var paused = player.isPaused()
+    
+    if (override){
+        playing = false;
+        paused = false;
+        override = false;
+    }
     if (playing){
         //alert("PLAYING: waiting for end of song");
         setTimeout(musicplayer1, 10000);
@@ -53,6 +74,8 @@ function musicplayer1()
     else{
         //alert("next song!");
         var index = getRandom();
+        
+        node.nodeValue = 'Song: ' + (links.length - items.length) + ' of ' + links.length;
         //var index = 15;
         links[index].dispatchEvent(theEvent);
         //links.splice(index, 1);
@@ -72,7 +95,7 @@ function makeItemArray(){
 //Gets random from index array, removes it, and returns index
 function getRandom(){
     if (items.length == 0){
-     	alert("All songs have been played!")
+     	//alert("All songs have been played!")
         items = makeItemArray();
     }
     var i = Math.floor(Math.random()*items.length)
@@ -80,4 +103,9 @@ function getRandom(){
     items.splice(i, 1);
 
     return item;  
+}
+
+function doOverride(){
+    override = true;
+    musicplayer1();
 }
