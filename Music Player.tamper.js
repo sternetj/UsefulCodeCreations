fbbea182be4a62bfc430e69a411b4ccd85512ec7
Teddy Sterne <sternetj@rose-hulman.edu>
2014-06-19 18:24:24 -0500

    Updated
    Made it so you can repeat songs and performed some code cleanup.

--- Music Player.tamper.js
+++ Music Player.tamper.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name       Music Player
 // @namespace  http://www.allaccess.com/top40-mainstream/cool-new-music
-// @version    0.1
-// @description  enter something useful
+// @version    2.0
+// @description  Allows you to control the player on allaccess.com. Also allows for continuous playing.
 // @match      http://www.allaccess.com/*/cool-new-music
 // @copyright  2012+, You
 // ==/UserScript==
@@ -14,63 +14,63 @@ var links = document.getElementsByClassName('play'); // get all links
 var theEvent = document.createEvent("MouseEvent");
 theEvent.initMouseEvent("click", true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
 
-// click the first link
-
-//var index = getRandom(0, links.length);
 //get first index
 var items = makeItemArray();
 var index = getRandom();
 
-//alert(index);
+//click the first link
 links[index].dispatchEvent(theEvent);
-//links.remove(index);
-//links[links.length - 24].dispatchEvent(theEvent);
 
-//alert(player.isPlaying());
-
+//check in on what the player is doing in 10 seconds
 setTimeout(musicplayer1, 10000);
 
 var elemDiv = document.createElement('div');
 elemDiv.style.cssText = 'position:fixed;top:5px;right:5px;width:145px;height:80px;z-index:10000;background:#eb295c;';
 elemDiv.style.cssText += 'text-align:center;vertical-align: middle;border-radius:8px;';
-//elemDiv.appendChild(elemP);
 document.body.appendChild(elemDiv);
 
 var para = document.createElement("p");
-para.style.cssText = 'padding-top:10px;padding-bottom:10px;font-weight:bold;font-family:Arial,Helvetica,sans-serif;color:#FFF;font-size: 16px;';
-var node = document.createTextNode('Song: 1 of ' + links.length);
-para.appendChild(node);
+para.style.cssText = 'padding-top:10px;padding-bottom:10px;font-weight:bold;font-family:Arial,Helvetica,sans-serif;';
+para.style.cssText += 'color:#FFF;font-size: 16px;';
+
+var song_string = document.createTextNode('Song: 1 of ' + links.length);
+
+para.appendChild(song_string);
 elemDiv.appendChild(para);
 
-//Back Arrow
-var anc2 = document.createElement("a");
-anc2.style.cssText = '-webkit-filter: invert(100%);padding-top:10px;padding-right:-5px;cursor: pointer; cursor: hand;font-size: 20px;';
-anc2.innerHTML = '<img src="http://www.flaticon.com/png/256/25641.png" alt="Prev" width="44" height="30">';
-anc2.addEventListener('click',doOverride,false);
-elemDiv.appendChild(anc2);
+var linkStyle = 'padding-top:10px;padding-right:-5px;cursor: pointer; cursor: hand;font-size: 20px;';
 
-//Pause
-var anc = document.createElement("a");
-anc.style.cssText = '-webkit-filter: invert(100%);padding-top:10px;cursor: pointer; cursor: hand;font-size: 20px;';
-//anc.innerHTML = 'Next';
-anc.innerHTML = '<img src="http://www.flaticon.com/png/256/25696.png" alt="Pause" width="44" height="30">';
-anc.addEventListener('click',doPlayPause,false);
-elemDiv.appendChild(anc);
+//Back Arrow
+var back = document.createElement("a");
+back.style.cssText = linkStyle + '-webkit-filter: invert(100%);';
+back.innerHTML = '<img src="http://www.flaticon.com/png/256/25641.png" alt="Prev" width="44" height="30">';
+back.addEventListener('click',doPlayPrevious,false);
+elemDiv.appendChild(back);
+
+//Play\Pause
+var play_pause = document.createElement("a");
+play_pause.style.cssText = linkStyle + '-webkit-filter: invert(100%);';
+play_pause.innerHTML = '<img src="http://www.flaticon.com/png/256/25696.png" alt="Pause" width="44" height="30">';
+play_pause.addEventListener('click',doPlayPause,false);
+elemDiv.appendChild(play_pause);
 
 //Next
-var anc3 = document.createElement("a");
-anc3.style.cssText = '-webkit-filter: invert(100%);padding-top:10px;padding-left:-5px;cursor: pointer; cursor: hand;font-size: 20px;';
-anc3.innerHTML = '<img src="http://www.flaticon.com/png/256/25309.png" alt="Prev" width="44" height="30">';
-anc3.addEventListener('click',doOverride,false);
-elemDiv.appendChild(anc3);
+var next = document.createElement("a");
+next.style.cssText = linkStyle + '-webkit-filter: invert(100%);';
+next.innerHTML = '<img src="http://www.flaticon.com/png/256/25309.png" alt="Prev" width="44" height="30">';
+next.addEventListener('click',doOverride,false);
+elemDiv.appendChild(next);
 
 var override = false;
+var played = [];
+var toPlay = [];
+var olderIndex = -1;
+
+played.push(index);
 
 
 function musicplayer1()
 {
-    //setTimeout(function() { alert('hello');}, 3000); //defer the execution of anonymous function for 3 seconds and go to next line of code.
-    //alert(player);
     var playing = player.isPlaying();
     var paused = player.isPaused();
     
@@ -80,21 +80,33 @@ function musicplayer1()
         override = false;
     }
     if (playing){
-        //alert("PLAYING: waiting for end of song");
+        //Change image to pause button
+        play_pause.innerHTML = '<img src="http://www.flaticon.com/png/256/25696.png" alt="Pause" width="30" height="30">';
         setTimeout(musicplayer1, 10000);
     }
     else if (paused){
-        //alert("PAUSED: waiting for no music queued");
+        //Change image to play button
+        play_pause.innerHTML = '<img src="http://www.flaticon.com/png/256/25226.png" alt="Play" width="30" height="30">';
         setTimeout(musicplayer1, 10000);
     }
     else{
-        //alert("next song!");
-        var index = getRandom();
+        olderIndex = index; // Set the last played song to allow for repeating songs
         
-        node.nodeValue = 'Song: ' + (links.length - items.length) + ' of ' + links.length;
-        //var index = 15;
+        //Check for songs to be played and play them first if they exist
+        if (toPlay.length > 0){
+            index = toPlay.pop();
+        }else{
+            index = getRandom();
+        }
+        
+        //Add the index to the played list
+        played.push(index);
+        
+        //Update the song string to reflect current track number
+        song_string.nodeValue = 'Song: ' + (links.length - items.length + Math.max(-1*toPlay.length, 0)) + ' of ' + links.length;
+        
+        //Click on the new son to have it start playing
         links[index].dispatchEvent(theEvent);
-        //links.splice(index, 1);
         
        	setTimeout(musicplayer1, 10000);
     }
@@ -131,11 +143,23 @@ function doPlayPause(){
     
     if (playing){
         player.pause();
-        anc.innerHTML = '<img src="http://www.flaticon.com/png/256/25226.png" alt="Pause" width="30" height="30">';
+        play_pause.innerHTML = '<img src="http://www.flaticon.com/png/256/25226.png" alt="Play" width="30" height="30">';
     }
     else {
         player.play();
-        anc.innerHTML = '<img src="http://www.flaticon.com/png/256/25696.png" alt="Pause" width="30" height="30">';
+        play_pause.innerHTML = '<img src="http://www.flaticon.com/png/256/25696.png" alt="Pause" width="30" height="30">';
     }
     musicplayer1();
+}
+
+function doPlayPrevious(){
+    if (played.length > 0){
+        override = true;
+        var tempIndex = played.pop();
+        toPlay.push(tempIndex);
+        if (index == tempIndex && olderIndex == tempIndex && played.length > 0){
+            toPlay.push(played.pop());
+        }
+    	musicplayer1();
+    }
 }
